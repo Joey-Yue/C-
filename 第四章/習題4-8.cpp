1260a82891464f7e74f409a78e27438ad2dfecd9
Joey-Yue <67743996+Joey-Yue@users.noreply.github.com>
2021-05-19 09:25:11 +0800

    Add 4-8
    2021/5/19

---
+++ 第四章/習題4-8.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+	float a
+	cout << "請出入成績\n"
+	cin  >> a
+
+	if (a >= 100)
+	{
+		cout << "成績不能大於100喔~~\n"
+	}
+	else if (a >= 90)
+	{
+		cout << "優\n"
+	}
+	else if (a >= 80)
+	{
+		cout << "甲\n"
+	}
+	else if (a >= 70)
+	{
+		cout << "乙\n"
+	}
+	else if (a >= 60)
+	{
+		cout << "丙\n"
+	}
+	else 
+	{
+		cout << "丁\n"
+	}
+
+    return 0;                        
+}
